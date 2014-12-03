887ba2f7c883a3b90aa6661877b4d6fc23736a18
Luiz Paulo Valenca <luizpaulo165@gmail.com>
2014-12-02 22:18:53 -0300

    home - cart list

--- layout/header.php
+++ layout/header.php
@@ -22,24 +22,46 @@
     <a href="#" id="cart">
       <span id="value-cart" class="t-center block f-blue-2">R$ 120,48</span>
     </a>
-    <div id="wrap-itens-cart">
-      <!-- carousel -->
-      <div class="jcarousel-wrapper">
-          <div class="jcarousel">
-              <ul>
-                  <li><img src="../_shared/img/img1.jpg" alt="Nome do produto"></li>
-                  <li><img src="../_shared/img/img2.jpg" alt="Nome do produto"></li>
-                  <li><img src="../_shared/img/img3.jpg" alt="Nome do produto"></li>
-                  <li><img src="../_shared/img/img4.jpg" alt="Nome do produto"></li>
-                  <li><img src="../_shared/img/img5.jpg" alt="Nome do produto"></li>
-                  <li><img src="../_shared/img/img6.jpg" alt="Nome do produto"></li>
-              </ul>
-          </div>
-          <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
-          <a href="#" class="jcarousel-control-next">&rsaquo;</a>
-          <p class="jcarousel-pagination"></p>
-      </div>
-      <!-- carousel -->
+  </div>
+  <div id="wrap-itens-cart" class="bg-white">
+    <!-- carousel -->
+    <div class="jcarousel-wrapper">
+        <div class="jcarousel">
+            <ul class="list-itens">
+                <li class="">
+                  <figure>
+                    <img src="images/temporary/banana.png" alt="banana"/>
+                  </figure>
+                  <span>Aliquam ac sem</span>
+                  <strong>R$  50,00 / kg</strong>
+                </li>
+                <li class="">
+                  <figure>
+                    <img src="images/temporary/manga.png" alt="banana"/>
+                  </figure>
+                  <span>Aliquam ac sem</span>
+                  <strong>R$  50,00 / kg</strong>
+                </li>
+                <li class="">
+                  <figure>
+                    <img src="images/temporary/feijao.png" alt="banana"/>
+                  </figure>
+                  <span>Aliquam ac sem</span>
+                  <strong>R$  50,00 / kg</strong>
+                </li>
+                <li class="">
+                  <figure>
+                    <img src="images/temporary/iogurte.png" alt="banana"/>
+                  </figure>
+                  <span>Aliquam ac sem</span>
+                  <strong>R$  50,00 / kg</strong>
+                </li>
+            </ul>
+        </div>
+        <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+        <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+        <p class="jcarousel-pagination"></p>
     </div>
+    <!-- carousel -->
   </div>
 </header>
