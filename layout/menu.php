194370acbaead8b77b38a8a2ebbf8aa4981f0d21
Luiz Paulo Valenca <luizpaulo165@gmail.com>
2014-12-08 19:09:55 -0300

    ajuste no menu

--- layout/menu.php
+++ layout/menu.php
@@ -1,8 +1,17 @@
 <ul>
   <li>
-    <a href="#">Cadastre-se</a>
+    <a href="#">Luiz Paulo</a>
   </li>
   <li>
-    <a href="#">Login</a>
+    <a href="cadastro.php">Cadastre-se</a>
+  </li>
+  <li>
+    <a href="configuracoes.php">Configurações</a>
+  </li>
+  <li>
+    <a href="cadastro.php">Login</a>
+  </li>
+  <li>
+    <a href="#">Sair</a>
   </li>
 </ul>
